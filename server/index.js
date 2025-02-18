098efbcd4e035d3ec8dfd3d8b6ab68d2ee85e3a5
gamandeepsingh <gamandeepsingh4@gmail.com>
2025-02-18 23:53:04 +0530

    remove error handling middleware and add Vercel configuration file

--- server/index.js
+++ server/index.js
@@ -12,10 +12,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something broke!", details: err.message });
-});
 
 
 app.use("/api/templates", require("./routes/route"));
